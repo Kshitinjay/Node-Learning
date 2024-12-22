320ad21f9b779a3a858601d4d6ba7ff2f7d6d7d1
Kshitinjay Kumar <kshitinjay20@gmail.com>
2024-12-22 21:27:07 +0530

    Created basic server and fetched data from local file and shared it in the response

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = [
+  { name: "Aryan", email: "aryan123@gmail.com" },
+  { name: "Meera", email: "meera.patel@yahoo.com" },
+  { name: "Rohan", email: "rohan.kumar@outlook.com" },
+  { name: "Sneha", email: "sneha.verma@gmail.com" },
+  { name: "Ishaan", email: "ishaan.malhotra@gmail.com" },
+  { name: "Anjali", email: "anjali.singh@hotmail.com" },
+  { name: "Vikram", email: "vikram.chauhan@gmail.com" },
+  { name: "Pooja", email: "pooja.sharma@yahoo.com" },
+  { name: "Aakash", email: "aakash.mehra@gmail.com" },
+  { name: "Priya", email: "priya.kapoor@hotmail.com" },
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require("http");
-const colors = require("colors");
-
+const data = require('./data')
 const callbackFunction = (req, resp) => {
-  console.log("Testing package".green);
-  resp.write("<h1>Hello world par 123</h1>");
+  resp.writeHead(200, { "Content-Type": "applicationjson" });
+  resp.write(
+    JSON.stringify(data)
+  );
   resp.end();
 };
-console.log("2".green);
+
 http.createServer(callbackFunction).listen(4200);
